18caba5897bb9e13d3558d06d011ebbb96578e23
LorenaTaddeo <l.deblek@gmail.com>
2023-05-10 17:36:25 +0200

    m2/s3/g3

---
+++ m2/s3/g3/script.js
@@ -0,0 +1,35 @@
+fetch('https://striveschool-api.herokuapp.com/books')
+.then((res) =>{
+    if(res.ok){
+        return res.json();
+    }else{
+        throw new Error('Error');
+    }
+})
+.then(dati => {
+    console.log(dati);
+    let row = document.querySelector("div.container-fluid div.row");
+    dati.forEach(element => {
+        row.innerHTML += 
+        `<div class="col">
+            <div class="card" style="width: 18rem;">
+                <img src="${element.img}" class="card-img-top">
+                <div class="card-body">
+                <h5 class="card-title">${element.title}</h5>
+                <p class="card-text"><span class="fw-bol">Category:</span>${element.category}</p>
+                <p class="card-text"><span class="fw-bol">Price:</span>${element.price}</p>
+                <button class="btn btn-dark id="discardButton">Discard</button>
+                </div>
+            </div>
+        </div>`   
+        }) 
+        
+    });
+
+
+let discard = col.querySelectorAll("#discardButton");
+for (let button of discard) {
+        button.addEventListener("click", () => {
+            button.parentElement.parentElement.parentElement.remove();
+        })
+    };
