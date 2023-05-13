276d3266187ec0ae8c28c1465d42d11d7150b110
LorenaTaddeo <l.deblek@gmail.com>
2023-05-13 23:20:17 +0200

    m2/s3/g5 progetto settimanale

---
+++ m2/s3/g5_progetto settimanale/indexscript.js
@@ -0,0 +1,68 @@
+const PRODUCTS = 'https://striveschool-api.herokuapp.com/api/product/'
+
+const hideColumn = function (context) {
+  console.log('context', context)
+  let rightColToDelete = context.closest('.cards')
+  /*rightColToDelete.remove()*/
+  fetch(PRODUCTS + context.id , {
+    method: 'DELETE',
+    headers: {
+      "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDVlMDRiYzg4Zjc0MDAwMTQyODc0ODMiLCJpYXQiOjE2ODM5ODExMjYsImV4cCI6MTY4NTE5MDcyNn0.4iidlSoK_Fm230r8gIjAlYPbZLm0duEMAfs2rh2n6vU",
+      "Content-Type": "application/json",
+      }
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+}
+
+const getProducts = function (){
+    fetch(PRODUCTS, {
+      headers: {
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDVlMDRiYzg4Zjc0MDAwMTQyODc0ODMiLCJpYXQiOjE2ODM5ODExMjYsImV4cCI6MTY4NTE5MDcyNn0.4iidlSoK_Fm230r8gIjAlYPbZLm0duEMAfs2rh2n6vU",
+        "Content-Type": "application/json",
+        }
+    })
+    .then((res) => {
+        console.log('RES', res)
+        if(res.ok){
+            return res.json()
+        } else {
+            throw new Error ('Errore nel recupero dei prodotti')
+        }
+    })
+    .then((data) => {
+        console.log('PRODOTTI IN DB', data)
+        data.forEach((product) => {
+            let colTemplate = `
+            <div class="col-12 col-md-3 cards">
+              <div class="card">
+              <img src="${product.imageUrl}" class="card-img-top w-100">
+                <div class="card-body">
+                  <h5 class="card-title">${product.name}</h5>
+                  <p class="card-text">${product.price}€</p>                  
+                  <p class="card-text">${product.description}</p>
+                  <p>${product.brand}</p>
+                  <a href="./details.html?productId=${product._id}" class="d-block mb-1">Scopri di più</a>
+                  <a href="./backoffice.html?productId=${
+                    product._id
+                  }" class="btn btn-primary">MODIFICA</a>
+                  <button type="button" class="btn btn-danger" onclick="hideColumn(this)" id="${product._id}">
+                  ELIMINA</button>
+                </div>
+              </div>
+            </div>
+            `;
+
+            let rowReference = document.getElementById("products-container");
+            rowReference.innerHTML += colTemplate;
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+window.onload = () => {
+  getProducts();
+};
